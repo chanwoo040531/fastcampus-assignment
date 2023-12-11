9e4cf89b0538291c6e598cc553a5a5fdbbd6cb2a
chanwoo040531 <chanwoo040531@gmail.com>
2023-12-12 05:29:57 +0900

    [feat] create customer domain

--- src/main/kotlin/me/chnu/fastcampusassignment/domain/customer/Customer.kt
+++ src/main/kotlin/me/chnu/fastcampusassignment/domain/customer/Customer.kt
@@ -2,11 +2,12 @@ package me.chnu.fastcampusassignment.domain.customer
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
+import jakarta.validation.constraints.NotNull
 import me.chnu.fastcampusassignment.domain.BaseEntity
 
 @Entity
 @Table(name = "customers")
 internal class Customer(
     var name: String,
-
+    var balance: Long,
 ) : BaseEntity()
--- src/main/kotlin/me/chnu/fastcampusassignment/domain/customer/CustomerRepository.kt
+++ src/main/kotlin/me/chnu/fastcampusassignment/domain/customer/CustomerRepository.kt
@@ -3,5 +3,4 @@ package me.chnu.fastcampusassignment.domain.customer
 import me.chnu.fastcampusassignment.domain.Key
 import org.springframework.data.jpa.repository.JpaRepository
 
-internal interface CustomerRepository : JpaRepository<Customer, Key> {
-}
+internal interface CustomerRepository : JpaRepository<Customer, Key>
