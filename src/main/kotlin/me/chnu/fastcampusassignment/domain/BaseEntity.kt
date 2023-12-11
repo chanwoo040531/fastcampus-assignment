eeb9e7dde03e889c4c93deeeaa293638589600c0
chanwoo040531 <chanwoo040531@gmail.com>
2023-12-12 07:29:15 +0900

    [fix] change access modifier of Key alias

--- src/main/kotlin/me/chnu/fastcampusassignment/domain/BaseEntity.kt
+++ src/main/kotlin/me/chnu/fastcampusassignment/domain/BaseEntity.kt
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
-typealias Key = Long
+internal typealias Key = Long
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
