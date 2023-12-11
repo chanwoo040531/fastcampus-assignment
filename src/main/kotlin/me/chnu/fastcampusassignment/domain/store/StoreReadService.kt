54bed8824af34ca4ffbd9d7354076975ef852107
chanwoo040531 <chanwoo040531@gmail.com>
2023-12-12 06:20:11 +0900

    [feat] create store read logic

---
+++ src/main/kotlin/me/chnu/fastcampusassignment/domain/store/StoreReadService.kt
@@ -0,0 +1,16 @@
+package me.chnu.fastcampusassignment.domain.store
+
+import me.chnu.fastcampusassignment.annotation.ReadService
+import me.chnu.fastcampusassignment.domain.Key
+import me.chnu.fastcampusassignment.domain.customer.CustomerRepository
+import me.chnu.fastcampusassignment.exception.NotFoundException
+import org.springframework.data.repository.findByIdOrNull
+
+@ReadService
+internal class StoreReadService(
+    private val storeRepository: StoreRepository
+) {
+
+    fun get(id: Key): Store = storeRepository.findByIdOrNull(id)
+        ?: throw NotFoundException("입력한 매장 정보와 일치하는 데이터가 없습니다.")
+}
