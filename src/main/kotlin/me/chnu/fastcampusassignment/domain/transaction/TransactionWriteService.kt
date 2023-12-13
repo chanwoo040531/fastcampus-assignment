654d7197478b9b3a0cb0ba4688ffa876fcbd2099
chanwoo040531 <chanwoo040531@gmail.com>
2023-12-14 00:05:37 +0900

    [feat] add payment logic

--- src/main/kotlin/me/chnu/fastcampusassignment/domain/transaction/TransactionWriteService.kt
+++ src/main/kotlin/me/chnu/fastcampusassignment/domain/transaction/TransactionWriteService.kt
@@ -1,27 +1,27 @@
 package me.chnu.fastcampusassignment.domain.transaction
 
 import me.chnu.fastcampusassignment.annotation.WriteService
+import me.chnu.fastcampusassignment.domain.Key
 import me.chnu.fastcampusassignment.domain.customer.Customer
 import me.chnu.fastcampusassignment.domain.payment.Payment
-import me.chnu.fastcampusassignment.exception.TransactionException
 
 @WriteService
 internal class TransactionWriteService(
     private val transactionRepository: TransactionRepository
 ) {
 
-    fun fail(customer: Customer, payment: Payment, message: String) {
+    fun fail(customer: Customer, payment: Payment, message: String): Key {
         val transaction = Transaction(
             status = TransactionStatus.FAIL,
             message = message,
             customer = customer,
             payment = payment,
         )
-        transactionRepository.save(transaction)
-        throw TransactionException(message = message)
+        val savedTransaction = transactionRepository.save(transaction)
+        return savedTransaction.id
     }
 
-    fun success(customer: Customer, payment: Payment) {
+    fun success(customer: Customer, payment: Payment): Key {
         val transaction = Transaction(
             status = TransactionStatus.SUCCESS,
             message = "결제에 성공하였습니다.",
@@ -29,6 +29,7 @@ internal class TransactionWriteService(
             payment = payment,
         )
 
-        transactionRepository.save(transaction)
+        val savedTransaction = transactionRepository.save(transaction)
+        return savedTransaction.id
     }
 }
