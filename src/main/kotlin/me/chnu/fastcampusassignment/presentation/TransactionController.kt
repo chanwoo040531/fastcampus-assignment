0f7eb1d590ce08e81c5a2275bf23bdde81b14538
chanwoo040531 <chanwoo040531@gmail.com>
2023-12-14 00:44:43 +0900

    [refactor] remove unused import directives

--- src/main/kotlin/me/chnu/fastcampusassignment/presentation/TransactionController.kt
+++ src/main/kotlin/me/chnu/fastcampusassignment/presentation/TransactionController.kt
@@ -2,18 +2,10 @@ package me.chnu.fastcampusassignment.presentation
 
 import me.chnu.fastcampusassignment.application.PaymentTransactionUseCase
 import me.chnu.fastcampusassignment.domain.Key
-import me.chnu.fastcampusassignment.domain.payment.Payment
-import me.chnu.fastcampusassignment.domain.payment.PaymentReadService
 import me.chnu.fastcampusassignment.domain.transaction.TransactionReadService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.net.URI
 
 @RestController
@@ -21,7 +13,6 @@ import java.net.URI
 internal class TransactionController(
     private val paymentTransactionUseCase: PaymentTransactionUseCase,
     private val transactionReadService: TransactionReadService,
-    private val paymentReadService: PaymentReadService,
 ) {
     @PostMapping("/execute/{payment-id}")
     fun execute(
